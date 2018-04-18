42a827277d62764436e5d23a2aa9c26d88e338dd
Michael Longauer <michael.longauer@groupbyinc.com>
2018-04-17 20:25:55 -0400

    update state file

--- src/api.js
+++ src/api.js
@@ -1,20 +1,78 @@
-export const API = {
-  1: [
+export const STAGES = {
+  0: [
     {
       image: 1,
       debt: 0,
       social: 0,
       stage: 1,
       debtTolerance: 2,
-      description: ''
+      description: 'A Turing machine is a hypothetical machine thought of by the mathematician Alan Turing in 1936. Despite its simplicity, the machine can simulate any computer algorithm, no matter how complicated it is!'
     }
   ],
-  2: [],
-  3: [],
-  4: [],
-  5: [],
-  6: [],
-  7: [],
+  1: [
+    {
+      image: 2,
+      debt: 0,
+      social: 0,
+      stage: 2,
+      debtTolerance: 2,
+      description: 'The term “Artificial Intelligence” was first coined by John McCarthy, a young computer scientist, in 1956 for a conference at Dartmouth University. It was discussed here how AI should be approached.'
+    }
+  ],
+  2: [
+    {
+      image: 3,
+      debt: 0,
+      social: 0,
+      stage: 4,
+      debtTolerance: 2,
+      description: 'Top-down programming tends to generate modules that are based on functionality, usually in the form of functions or procedures. Typically, the high-level specification of the system states functionality. This high-level description is then refined to be a sequence or a loop of simpler functions or procedures, that are then themselves refined, etc.'
+    },
+    {
+      image: 4,
+      debt: 1,
+      social: 0,
+      stage: 3,
+      debtTolerance: 0,
+      description: 'An approach to development which attempts to simulate networks of artificial neurons that are similar to the neurons in the human brain. They then investigate what aspects of cognition can be recreated in these artificial networks.'
+    }
+  ],
+  3: [
+    {
+      paragraph: '<p>Not enough technology. Gain 1 charge of <span>Debt</span>.',
+      stage: 2
+    }
+  ],
+  4: [
+    {
+      image: 5,
+      debt: 1,
+      social: 0,
+      stage: 5,
+      debtTolerance: 2,
+      description: 'Shakey could make a map of the area around it and navigate through it. Although it seems advanced, it was painfully slow, and had to update its map every time it moved a little bit forward for up to an hour.'
+    }
+  ],
+  5: [
+    {
+      paragraph: '<p>Failure. AI Winter initiates. Gain 1 charge of <span>Debt</span>.',
+      stage: 1
+    }
+  ],
+  6: [
+    {
+      image: 6,
+      debt: 0,
+      social: 0,
+      stage: 7,
+      debtTolerance: 2,
+      description: 'RI was the first successful commercial expert system. It configured orders for new computer systems. After 5 years, it was saving the company about 40 million dollars per year.'
+    }
+    // figure out bonus button
+  ],
+  7: [
+      
+  ],
   8: [],
   9: [],
   10: [],
